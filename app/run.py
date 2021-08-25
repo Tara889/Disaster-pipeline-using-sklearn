7ad75ab50ee0032dd6a0e78b51ab4c76a2832668
Tara Devi <nakshatra1988@gmail.com>
2021-08-25 17:55:26 +0530

    pushing changes

--- app/run.py
+++ app/run.py
@@ -35,10 +35,10 @@ def tokenize(text):
 
 # load data
 engine = create_engine('sqlite:///../data/DisasterResponse.db')
-df = pd.read_sql_table('FigureEight', engine)
+df = pd.read_sql_table('InsertTableName', engine)
 
 # load model
-model = joblib.load("../models/tara.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
